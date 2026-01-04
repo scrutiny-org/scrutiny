c3bae70155c618cd0dc3505ad66ab268c8feba5d
delacor <robin.tschirschnitz@googlemail.com>
2026-01-04 23:30:54 +0100

    Many code-coverage tinkering

--- webapp/frontend/src/app/layout/common/detail-settings/detail-settings.component.spec.ts
+++ webapp/frontend/src/app/layout/common/detail-settings/detail-settings.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DetailSettingsComponent } from './detail-settings.component';
 
@@ -7,11 +11,21 @@ describe('DetailSettingsComponent', () => {
   let component: DetailSettingsComponent;
   let fixture: ComponentFixture<DetailSettingsComponent>;
 
+  const matDialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatDialogModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatButtonModule,
+        NoopAnimationsModule
+      ],
       declarations: [ DetailSettingsComponent ],
       providers: [
-        { provide: MAT_DIALOG_DATA, useValue: { curMuted: false } }
+        { provide: MAT_DIALOG_DATA, useValue: { curMuted: false } },
+        { provide: MatDialogRef, useValue: matDialogRefSpy }
       ]
     })
     .compileComponents();
